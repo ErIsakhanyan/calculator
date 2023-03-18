0a61ead1673f261c306654eb50f7e60f742c150e
Arman <erikisakhanyan07@gmail.com>
2023-03-18 11:55:46 +0400

    Added app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+let name = prompt("Enter name: ");
+
+const areYouFoolish = () => {
+    if(name === "Ero" || name === "Yuro") {
+        alert("Yes you are")
+    } else {
+        alert("No you are not")
+    }
+}
